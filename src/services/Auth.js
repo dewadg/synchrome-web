0eddf354d7bad70ded0fc3b97d5cfcee03e23538
Adhyaksa Jaya <adhyaksaj@gmail.com>
2018-11-05 00:38:53 +0700

    Set user data after logged in

--- src/services/Auth.js
+++ src/services/Auth.js
@@ -1,3 +1,5 @@
+import store from '../store'
+
 export default class Auth {
   _http = null
 
@@ -25,8 +27,27 @@ export default class Auth {
         username: resp.data.username,
         password: resp.data.password
       }
+
+      store.commit('LoggedUser/set', await this.whoami())
     } catch (err) {
       throw new Error('Nama pengguna/kata sandi salah')
+    }
+  }
+  
+  /**
+   * Fetch current authenticated user from API.
+   */
+  async whoami () {
+    if (!this._http) {
+      throw new Error('HTTP client is not set')
+    }
+
+    try {
+      const resp = await this._http.get('whoami')
+
+      return resp.data.data
+    } catch (err) {
+      throw new Error('Failed while fetching authenticated user')
     }
   }
 }
