75e2d4c52122e1c88c79a54ee654e4471f7c8ab5
Adhyaksa Jaya <adhyaksaj@gmail.com>
2018-12-28 13:40:50 +0700

    Added attendance type service

---
+++ src/services/AttendanceType.js
@@ -0,0 +1,20 @@
+import { removeDataNamespace } from '@/helpers/data'
+
+export default class AttendanceType {
+  _http = null
+
+  constructor(httpService) {
+    this._http = httpService
+  }
+
+  async get() {
+    try {
+      const res = await this._http.get('attendance-types')
+
+      return removeDataNamespace(res.data)
+    } catch (err) {
+      console.error(err)
+      throw new Error('Gagal mengambil data jenis absensi')
+    }
+  }
+}
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 import Http from './Http'
 import Auth from './Auth'
 import Rank from './Rank'
+import AttendanceType from './AttendanceType'
 
 export const httpService = new Http()
 export const authService = new Auth(httpService)
 export const rankService = new Rank(httpService)
+export const attendanceTypeService = new AttendanceType(httpService)
