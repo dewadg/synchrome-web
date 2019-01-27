29dd9c884096222d32e2fc6538be3d9fa49ca8d1
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-01-27 14:57:00 +0700

    Rewritten AttendanceType store

--- src/stores/AttendanceType.js
+++ src/stores/AttendanceType.js
@@ -1,43 +1,9 @@
-import { attendanceTypeService } from '../services'
-import { schema, normalize, denormalize } from 'normalizr'
-
-export const attendanceTypeSchema = new schema.Entity('attendanceTypes')
-export const attendanceTypeListSchema = [ attendanceTypeSchema ]
-
-const state = {
-  data: {
-    entities: {},
-    result: []
-  }
-}
-
-const mutations = {
-  setData (state, data) {
-    state.data = normalize(data, attendanceTypeListSchema)
-  }
-}
-
-const getters = {
-  getData (state) {
-    return denormalize(state.data.result, attendanceTypeListSchema, state.data.entities)
-  }
-}
-
-const actions = {
-  async fetchAll (context) {
-    try {
-      const attendanceTypes = await attendanceTypeService.get()
-      context.commit('setData', attendanceTypes)
-
-      return context.getters.getData
-    } catch (err) {
-      throw new Error('Terjadi kesalahan ketika mengambil data jenis presensi')
-    }
-  }
-}
+import state from './states/attendanceType'
+import getters from './getters/attendanceType'
+import mutations from './mutations/attendanceType'
+import actions from './actions/attendanceType'
 
 export default {
-  namespaced: true,
   state,
   getters,
   mutations,
