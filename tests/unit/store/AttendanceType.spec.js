41a25d820465674970cbe0ae2ddd00cb17d14346
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-01-27 15:01:25 +0700

    Added unit test for AttendanceType store

---
+++ tests/unit/store/AttendanceType.spec.js
@@ -0,0 +1,25 @@
+import actions from '@/stores/actions/attendanceType'
+import sinon, { assert } from 'sinon'
+import { authService } from '@/services'
+import {
+  FETCH_ALL_ATTENDANCE_TYPES,
+  FETCH_ALL_ATTENDANCE_TYPES_SUCCESS
+} from '../../../src/stores/types/attendanceType'
+
+describe('AttendanceType Store', () => {
+  before(async () => {
+    await authService.authenticate({
+      name: process.env.VUE_APP_AUTH_USERNAME,
+      password: process.env.VUE_APP_AUTH_PASSWORD
+    })
+  })
+
+  it(`should dispatch ${FETCH_ALL_ATTENDANCE_TYPES} successfully`, async () => {
+    const commit = sinon.spy()
+
+    await actions[FETCH_ALL_ATTENDANCE_TYPES]({ commit })
+
+    assert.match(commit.getCall(0).args[0], FETCH_ALL_ATTENDANCE_TYPES)
+    assert.match(commit.getCall(1).args[0], FETCH_ALL_ATTENDANCE_TYPES_SUCCESS)
+  })
+})
