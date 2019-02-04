3d7855c872a343d31c2ba31adea09b3b29bde2ed
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-02-05 01:21:38 +0700

    Fixed unauthorized bug on every request

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,5 @@
 import store from '@/store'
+import { httpService } from '@/services'
 import { getSession } from '@/helpers/session'
 import { TOKEN_SESSION } from '@/constants/session'
 import { FETCH_LOGGED_USER } from '@/stores/types/loggedUser'
@@ -7,7 +8,10 @@ export async function auth (to, from, next) {
   if (!getSession(TOKEN_SESSION)) {
     next({ name: 'login' })
   } else {
-    await store.dispatch(FETCH_LOGGED_USER)
+    const fetchUserHandler = store.dispatch(FETCH_LOGGED_USER)
+
+    httpService.authData = JSON.parse(getSession(TOKEN_SESSION))
+    await fetchUserHandler
 
     next()
   }
