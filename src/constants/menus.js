632baf3b7de9c209d7cc5a9b83fdbbbc68556dd4
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-02-10 16:32:14 +0700

    Restructured AppNav menus

---
+++ src/constants/menus.js
@@ -0,0 +1,68 @@
+export default [
+  {
+    to: 'dashboard',
+    label: 'Dasbor',
+    icon: 'dashboard'
+  },
+  {
+    divider: true
+  },
+
+  // Master Data
+  {
+    subheader: 'Data Utama'
+  },
+  {
+    to: 'opd',
+    label: 'OPD',
+    icon: 'home'
+  },
+  {
+    to: 'echelons',
+    label: 'Eselon',
+    icon: 'group'
+  },
+  {
+    to: 'ranks',
+    label: 'Golongan',
+    icon: 'group'
+  },
+  {
+    to: 'functional-positions',
+    label: 'Jabatan Fungsional',
+    icon: 'work'
+  },
+  {
+    to: 'asn',
+    label: 'ASN',
+    icon: 'group'
+  },
+  {
+    to: 'calendars',
+    label: 'Kalender Kerja',
+    icon: 'event'
+  },
+  {
+    to: 'workshifts',
+    label: 'Shift Kerja',
+    icon: 'schedule'
+  },
+  {
+    divider: true
+  },
+
+  // App
+  {
+    subheader: 'Pengaturan'
+  },
+  {
+    to: 'users',
+    label: 'Pengguna',
+    icon: 'group'
+  },
+  {
+    to: 'configs',
+    label: 'Konfigurasi',
+    icon: 'settings'
+  }
+]
