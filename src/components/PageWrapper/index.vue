899bd9b83fbfde5b0c9d9cf167e3285c018a7ef9
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-02-17 20:54:36 +0700

    Added breadcrumbs prop to PageWrapper

--- src/components/PageWrapper/index.vue
+++ src/components/PageWrapper/index.vue
@@ -6,6 +6,19 @@
       justify-center
     >
       <VFlex
+        v-if="breadcrumbs.length > 0"
+        lg11
+        xs12
+        class="mb-4"
+      >
+        <VBreadcrumbs
+          :items="breadcrumbs"
+          class="pa-0"
+        >
+          <VIcon slot="divider">chevron_right</VIcon>
+        </VBreadcrumbs>
+      </VFlex>
+      <VFlex
         lg11
         md12
         xs12
@@ -15,3 +28,17 @@
     </VLayout>
   </VContainer>
 </template>
+
+<script>
+export default {
+  name: 'PageWrapper',
+
+  props: {
+    breadcrumbs: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
