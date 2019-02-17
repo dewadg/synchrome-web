6ed895cb361e10b99b30a623ffc4ceaf81c256a8
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-02-17 16:10:44 +0700

    Added date helper

---
+++ src/helpers/date.js
@@ -0,0 +1,18 @@
+export function dayByIndex (value) {
+  switch (value) {
+    case 1:
+      return 'Senin'
+    case 2:
+      return 'Selasa'
+    case 3:
+      return 'Rabu'
+    case 4:
+      return 'Kamis'
+    case 5:
+      return 'Jumat'
+    case 6:
+      return 'Sabtu'
+    case 7:
+      return 'Minggu'
+  }
+}
