aad83bd5975fa0d277641719492bb3fe511c89af
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-03-18 21:09:52 +0700

    Added agency store

---
+++ src/stores/Agency.js
@@ -0,0 +1,11 @@
+import state from './states/agencyState'
+import getters from './getters/agencyGetters'
+import mutations from './mutations/agencyMutations'
+import actions from './actions/agencyActions'
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+}
