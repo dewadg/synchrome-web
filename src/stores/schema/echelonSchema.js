1162cb10834bc7a9507705a5440f091b68e11a11
Adhyaksa Jaya <adhyaksaj@gmail.com>
2019-03-23 17:19:32 +0700

    Fixed bug on EchelonEdit page

--- src/stores/schema/echelonSchema.js
+++ src/stores/schema/echelonSchema.js
@@ -1,7 +1,10 @@
 import { schema } from 'normalizr'
 import { ECHELON_TYPE_SCHEMA } from './echelonTypeSchema'
 
+export const SUPERVISOR_SCHEMA = new schema.Entity('echelons')
+
 export const ECHELON_SCHEMA = new schema.Entity('echelons', {
-  type: ECHELON_TYPE_SCHEMA
+  type: ECHELON_TYPE_SCHEMA,
+  supervisor: SUPERVISOR_SCHEMA
 })
 export const ECHELON_LIST_SCHEMA = [ECHELON_SCHEMA]
